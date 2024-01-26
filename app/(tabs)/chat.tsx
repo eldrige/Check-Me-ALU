a376532e9321d5ea5d4e15f0cafe7ded463a57a8
eldrige <peldriige8@gmail.com>
2024-01-27 00:08:40 +0200

    style: chat-screeen

--- app/(tabs)/chat.tsx
+++ app/(tabs)/chat.tsx
@@ -1,13 +1,34 @@
-import { StyleSheet, TextInput } from 'react-native';
+import { Keyboard, StyleSheet, TextInput } from 'react-native';
 
-import EditScreenInfo from '@/components/EditScreenInfo';
+import DateTimePicker, {
+  DateTimePickerAndroid,
+} from '@react-native-community/datetimepicker';
 import { Text, View } from '@/components/Themed';
 import { Ionicons } from '@expo/vector-icons';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { ScrollView } from 'react-native-gesture-handler';
 import { router } from 'expo-router';
+import { useState } from 'react';
 
 export default function ChatScreen() {
+  const [date, setDate] = useState(new Date(1598051730000));
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState<Array<string>>([
+    'Hii, there I am the main man',
+  ]);
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate;
+    setDate(currentDate);
+  };
+
+  const handleSendMessage = () => {
+    if (message) {
+      setMessages([...messages, message]);
+      setMessage('');
+    }
+    Keyboard.dismiss();
+  };
+
   return (
     <SafeAreaView className="w-full flex-1">
       <View className="w-full flex flex-row items-center justify-between px-4 py-2">
@@ -23,16 +44,46 @@ export default function ChatScreen() {
           </View>
           <Text>Jane Doe</Text>
         </View>
-        <Ionicons name="calendar" size={24} color="black" />
+        <Ionicons
+          name="calendar"
+          size={24}
+          color="black"
+          onPress={() => {
+            DateTimePickerAndroid.open({
+              value: date,
+              onChange,
+              mode: 'date',
+              is24Hour: true,
+            });
+          }}
+        />
       </View>
-      <ScrollView className="flex-1"></ScrollView>
+      <ScrollView className="flex-1 px-4 py-2 gap-2">
+        {messages.map((message, index) => {
+          return (
+            <Text
+              key={index}
+              className="w-3/5 px-4 py-2 bg-red-200 rounded-t-lg rounded-bl-lg self-end"
+            >
+              {message}
+            </Text>
+          );
+        })}
+      </ScrollView>
       <View className="w-full flex flex-row items-center justify-between px-4 py-3 gap-2 bg-gray-100">
         <Ionicons name="add" size={28} color="black" />
         <TextInput
           className="flex-1 bg-gray-300 py-2 px-4 rounded-3xl "
           placeholder="Type your message"
+          value={message}
+          onChangeText={(val) => setMessage(val)}
         />
-        <Ionicons name="paper-plane" size={24} color="black" />
+        <Ionicons
+          name="paper-plane"
+          size={24}
+          color="black"
+          onPress={handleSendMessage}
+        />
       </View>
     </SafeAreaView>
   );
